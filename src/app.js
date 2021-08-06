1955388fa5b1f187870b64c371cbdbe68e623e09
Kiarra Gibbs <kiarra.gibbs@gmail.com>
2021-08-05 21:23:14 -0400

    added cors package

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
@@ -24,3 +27,5 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
+
+
